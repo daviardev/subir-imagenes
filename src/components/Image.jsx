61617c6935e8938a5ba7b8038f834e00aa991834
jersonsilvaa <jerson.silva99@hotmail.com>
2022-06-28 13:02:58 -0500

    #3 [Terminado estructurado]

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "../styles/componets/Image.scss"
 
 const Image = () => {
+    const PreviewImage = ({ src }) => <img src={src} />
     return (
-        <>
-            <h2>Image Preview</h2>
-        </>
+        <section className='preview'>
+        <h2 className='preview__title'>Última imagen subida</h2>
+
+        <div className='preview__container'>
+            <PreviewImage src={'https://cdn.pixabay.com/photo/2020/09/23/19/58/halloween-5596921_960_720.jpg'} />
+        </div>
+    </section>
     )
 }
 
